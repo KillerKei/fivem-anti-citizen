8f43aae942673ea9c153f699d3f03c8c45d1239c
Keiron <73769314+KillerKei@users.noreply.github.com>
2022-03-12 16:39:34 +0000

    Update sv_damage.lua

--- keiron-anticitizen/server/sv_damage.lua
+++ keiron-anticitizen/server/sv_damage.lua
@@ -1,4 +1,6 @@
 RegisterServerEvent('infinite-anticheat:funTime', function(dmg, acc)
     local src = source
-    TriggerClientEvent('infinite-anticheat:receivedBan', src, {reason = "Damage - " .. dmg .. " Accuracy - " .. acc, trigger = "Citizen Freak"})
+    local reason = 'Citizen Modifications | Damage: ' .. dmg .. ' | Accuracy ' .. acc
+    DropPlayer(src, reason)
+--     TriggerClientEvent('infinite-anticheat:receivedBan', src, {reason = "Damage - " .. dmg .. " Accuracy - " .. acc, trigger = "Citizen Freak"})
 end)    
